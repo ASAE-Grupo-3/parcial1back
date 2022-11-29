e7a8a2a9f2652e9ff26651b32ee1b11b500c890c
juancarloss9812 <juancarloss@unicauca.edu.co>
2022-11-29 14:58:32 -0500

    Add path variable and request Body for parents

--- src/main/java/co/edu/unicauca/distribuidos/core/proyecto/controllers/EstudianteRestController.java
+++ src/main/java/co/edu/unicauca/distribuidos/core/proyecto/controllers/EstudianteRestController.java
@@ -6,19 +6,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.unicauca.distribuidos.core.proyecto.services.DTO.EstudianteDTO;
 import co.edu.unicauca.distribuidos.core.proyecto.services.services.clienteServices.IEstudianteService;
 
-import org.springframework.validation.annotation.Validated;
 
 @RestController
 @RequestMapping("/api")
-@Validated
 public class EstudianteRestController {
 
 	@Autowired
@@ -30,21 +30,21 @@ public class EstudianteRestController {
 	}
 
 	@GetMapping("/estudiantes/{id}")
-	public EstudianteDTO show( Integer id) {
+	public EstudianteDTO show(@PathVariable Integer id) {
 		EstudianteDTO objCliente = null;
 		objCliente = EstudianteService.findById(id);
 		return objCliente;
 	}
 
 	@PostMapping("/estudiantes")
-	public EstudianteDTO create( EstudianteDTO cliente) {
+	public EstudianteDTO create(@RequestBody EstudianteDTO cliente) {
 		EstudianteDTO objCliente = null;
 		objCliente = EstudianteService.save(cliente);
 		return objCliente;
 	}
 
 	@PutMapping("/estudiantes/{id}")
-	public EstudianteDTO update( EstudianteDTO cliente, Integer id) {
+	public EstudianteDTO update(@RequestBody EstudianteDTO cliente, @PathVariable Integer id) {
 		EstudianteDTO objCliente = null;
 		System.out.println("actualizando cliente");
 		EstudianteDTO clienteActual = EstudianteService.findById(id);
@@ -55,7 +55,7 @@ public class EstudianteRestController {
 	}
 
 	@DeleteMapping("/estudiantes/{id}")
-	public Boolean delete( Integer id) {
+	public Boolean delete(@PathVariable Integer id) {
 		Boolean bandera = false;
 		EstudianteDTO clienteActual = EstudianteService.findById(id);
 		if (clienteActual != null) {
